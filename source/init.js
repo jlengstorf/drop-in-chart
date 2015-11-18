6dea3c7a84ee776b02b71555434bd048b50f9b59
jlengstorf <jason.lengstorf@copterlabs.com>
2015-11-18 08:15:35 +0700

    feat(docs): added inline documentation

--- source/init.js
+++ source/init.js
@@ -1,13 +1,26 @@
-// # Front-End Initialization
-
+/*
+ * # Front-End Initialization
+ * First, we need `react` and `react-dom` loaded.
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
+ * Then we load the `DropInChart` component.
+ */
 import DropInChart from './js/components/DropInChart';
 
-// Loads the stylesheet (via webpack)
-// require('./css/main.css');
-
+/*
+ * To allow us to drop in the component wherever, we need to attach the libs to
+ * the `window` so we can access they globally. Kind of shitty, but I can't
+ * think of a simpler way to solve the problem.
+ */
 window.React = React;
 window.ReactDOM = ReactDOM;
 window.DropInChart = DropInChart;
+
+/*
+ * Worth noting is that nothing really happens (aside from polluting the global
+ * namespace) in this script. Our goal here is to package up all the
+ * dependencies in a way that makes it easy to drop a chart into any page.
+ */
